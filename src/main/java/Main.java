452b371acf70e049900fa0442964865b9a67148f
ViniciusNunes01 <sanjip415@gmail.com>
2026-03-01 19:36:29 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,17 @@ public class Main {
 			System.out.print("$ ");
 			String command = sc.nextLine();
 
-			if (command.equals("exit")) {
+			if (command.startsWith("type")) {
+				if (command.substring(5).startsWith("echo")) {
+					System.out.println("echo is a shell builtin");
+				} else if (command.substring(5).startsWith("exit")) {
+					System.out.println("exit is a shell builtin");
+				} else if (command.substring(5).startsWith("type")) {
+					System.out.println("type is a shell builtin");
+				} else {
+					System.out.println(command.substring(5) + ": not found");
+				}
+			} else if (command.equals("exit")) {
 				break;
 			} else if (command.startsWith("echo")) {
 				System.out.println(command.substring(5));
