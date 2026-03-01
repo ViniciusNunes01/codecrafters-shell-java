9813aa8765019423aae4f601e9388ba192aa2598
ViniciusNunes01 <sanjip415@gmail.com>
2026-03-01 18:52:45 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 
+		Scanner sc = new Scanner(System.in);
+
 		System.out.print("$ ");
+
+		String command = sc.nextLine();
+		System.out.print(command + ": command not found");
+
+		sc.close();
 	}
 }
